f6a0b35b19004e4e160161f4895d89ae6c4d6389
AlvaroL <lavado.mejia.alvaro.elias@gmail.com>
2026-09-12 13:07:00 -0500

    feat: agregar el .gitignore por node

---
+++ api/index.js
@@ -0,0 +1,13 @@
+const express= require('express');
+const app= express();
+
+const PORT= process.env.PORT || 3000;
+const MESSAGE = process.env.MESSAGE || 'Nombre no configurado';
+
+app.get('/', (req, res) => {
+    res.json({ mensaje: `Hola, soy la API de alvaritol` });
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
